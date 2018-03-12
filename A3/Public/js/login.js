bd948c4b522f2d9c1aa94f77c5a468b521a0fbc2
phabibi <phabibi@asb9838nu-c15.csil.sfu.ca>
2018-03-11 17:31:58 -0700

    server is up and styling

---
+++ A3/Public/js/login.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  console.log("calling GET /checkin");
+  $("#checkin").click(function()
+{
+
+
+  $.ajax({
+    method: 'get',
+    url: '/checkin',
+    success: function()
+    {
+      window.location.pathname = "checkin"
+    }
+  });
+  });
+});
--- A3/server.js
+++ A3/server.js
@@ -3,44 +3,120 @@ var express = require('express');
 var app = express();
 var serverIndex = require('serve-index');
 var http = require('http');
+var path = require('path');
+var session =require('express-session');
+
+var username;
+var pass;
 
 var port = process.env.PORT || 3000;
 var users = [];
+var ssn;
 
 // parsing body
 app.use(express.json());
 app.use(express.urlencoded( { extended:false} ));
 
+
+app.use(express.static(path.join(__dirname,'/Public/')));
+app.use(session({secret:'PASWAG'}));
+
 var options = {
   dotfiles: 'ignore',
   etag: false,
   extensions: ['htm','html'],
-  index: "start.html"
+  index: "login.html"
 }
+
+var admin_option= {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm','html'],
+  index: "admin.html"
+}
+
 
 app.use('/', function(req,res,next){
   console.log(req.method, 'request:', req.url, JSON.stringify(req.body));
   next();
 });
 
-// app.all('/', function(req,res,next){
-//   console.log('Accessing root folder');
-//   next();
-// });
 
-app.use('/', express.static('./login', options));
 
-app.get('/users-api', function(req,res,next){
-  // serve users as json
-  res.json(users);
+
+app.get('/', function(req,res,next){
+  console.log("checking session");
+  if(ssn == null)
+  {
+    console.log("setting session");
+    ssn = req.session;
+  }
+  console.log("serving page");
+
+  res.sendFile(__dirname + "/login.html");
+
 });
-
 //this should be done using ajax.
-app.post('/users-api', function(req,res,next){
-  console.log(req.body);
-  users.push(req.body);
-  res.json(users);
+app.post('/', function(req,res,next){
+  console.log("setting the session params");
+  ssn = req.session;
+  username = req.body.user;
+  pass = req.body.pass;
+
+  ssn.user = username;
+  ssn.pass = pass;
+
+  if(ssn.user === 'admin' && ssn.pass === '1234' )
+  {
+    console.log("loging in adming user");
+    return res.redirect('/admin');
+
+  }
+
+  else {
+    console.log("the user is not an admin");
+    return res.redirect('/');
+    }
 });
+
+app.get('/admin',function(req,res,next)
+
+{
+  if(ssn == null)
+  {
+    console.log("first timer");
+    return res.redirect("/");
+
+
+  }
+  else{
+    console.log("buddy already logged on");
+
+    ssn = req.session;
+
+    if(ssn.user === 'admin' && ssn.pass === '1234' )
+    {
+      console.log("loging buddy in");
+      return res.sendFile(__dirname + "/admin.html");
+    }
+
+    else
+     {
+       console.log("my mans not an admin")
+       return res.redirect("/");
+     }
+
+  }
+  next();
+});
+
+app.get('/checkin',function(req,res)
+
+{
+  console.log("checking in ovee heeii");
+  return res.sendFile(__dirname + "/checkin.html");
+});
+  //console.log(req.body);
 
 app.delete('/users-api/:id', function(req,res,next){
   // search database for id
